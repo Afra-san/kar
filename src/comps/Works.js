24f2078e001939682a067333acf6b3f2200b16fc
Mohamad Afra <103582180+Afra-san@users.noreply.github.com>
2022-08-26 18:59:14 +0430

    Update Works.js

--- src/comps/Works.js
+++ src/comps/Works.js
@@ -9,34 +9,34 @@ export default function Works() {
             <div className="flex flex-col justify-between w-full lg:flex-row items-center lg:items-center">
                 <motion.div>
                   <div className="img-1 box-size radius"></div>
-                  <h2 className="text-start text-2xl my-4">Portofolio v1.0</h2>
+                  <h2 className="text-start text-2xl my-4">Portofolio v1.0 <a className='font-normal text-xl ml-4 mr-2' href='https://github.com/Afra-san/portofolio-V-1.0'>Repo</a>/<a className='font-normal text-xl ml-2' href='https://afra0.netlify.app/'>Live Demo</a></h2>
                   <p className="text-start font-light mb-8">React js</p>
                 </motion.div>
                 <div>
                   <div className="img-2 box-size bg-[#D9D9D9] radius"></div>
-                  <h2 className="text-start text-2xl my-4">Portofolio v2.0 beta</h2>
+                  <h2 className="text-start text-2xl my-4">Portofolio v2.0 beta <a className='font-normal text-xl ml-4 mr-2' href='https://github.com/Afra-san/V2.0beta'>Repo</a>/<a className='font-normal text-xl ml-2' href='https://afra-v2beta.netlify.app/'>Live Demo</a></h2>
                   <p className="text-start font-light mb-8">React js</p>
                 </div>
                 <div>
                   <div className="img-3 box-size bg-[#D9D9D9] radius"></div>
-                  <h2 className="text-start text-2xl my-4">Shopping Cart</h2>
+                  <h2 className="text-start text-2xl my-4">Shopping Cart <a className='font-normal text-xl ml-4 mr-2' href='https://github.com/Afra-san/Cart'>Repo</a>/<a className='font-normal text-xl ml-2' href='https://afra-cart.netlify.app/'>Live Demo</a></h2>
                   <p className="text-start font-light mb-8">React js , Context</p>
                 </div>
             </div>
             <div className="flex flex-col justify-between w-full lg:flex-row items-center lg:items-center">
                 <div>
                   <div className="img-4 box-size bg-[#D9D9D9] radius"></div>
-                  <h2 className="text-start text-2xl my-4 font-medium">Prototype </h2>
+                  <h2 className="text-start text-2xl my-4 font-medium">Prototype <a className='font-normal text-xl ml-4 mr-2' href='#'>Repo</a>/<a className='font-normal text-xl ml-2' href='#'>Live Demo</a> </h2>
                   <p className="text-start font-light">React js , Next js</p>
                 </div>
                 <div>
                   <div className="img-5 box-size bg-[#D9D9D9] radius"></div>
-                  <h2 className="text-start text-2xl my-4 font-medium">Dashboard</h2>
+                  <h2 className="text-start text-2xl my-4 font-medium">Dashboard <a className='font-normal text-xl ml-4 mr-2' href='https://github.com/Afra-san/dash'>Repo</a>/<a className='font-normal text-xl ml-2' href='https://dash-afra.netlify.app/'>Live Demo</a></h2>
                   <p className="text-start font-light">React js</p>
                 </div>
                 <div>
                   <div className="img-6 box-size bg-[#D9D9D9] radius"></div>
-                  <h2 className="text-start text-2xl my-4 font-medium">Rimakii</h2>
+                  <h2 className="text-start text-2xl my-4 font-medium">Rimakii Brand <a className='font-normal text-xl ml-4 mr-2' href='#'>Repo</a>/<a className='font-normal text-xl ml-2' href='#'>Live Demo</a></h2>
                   <p className="text-start font-light">React js , Figma</p>
                 </div>
             </div>
